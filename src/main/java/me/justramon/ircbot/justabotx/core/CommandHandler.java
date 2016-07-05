0546136612e091f6d02f5ef89804e7354e2129c7
JustRamon <justramon@outlook.com>
2016-07-05 18:54:45 +0200

    Fixing "not playing a game" msg when not op

--- src/main/java/me/justramon/ircbot/justabotx/core/CommandHandler.java
+++ src/main/java/me/justramon/ircbot/justabotx/core/CommandHandler.java
@@ -107,48 +107,54 @@ public class CommandHandler extends ListenerAdapter
 		{
 			String channel = event.getChannel().getName();
 
-			if(cmdName.equalsIgnoreCase("@disable") && Operators.isOp(event))
+			if(cmdName.equalsIgnoreCase("@disable"))
 			{
-				if(GameModeHandler.isPlaying(channel))
+				if(Operators.isOp(event))
 				{
-					GameModeHandler.disableGameMode(channel);
-					System.gc();
-				}
-				else
-				{	
-					MessageHandler.respond(event, "This channel is currently not playing a game.");
+					if(GameModeHandler.isPlaying(channel))
+					{
+						GameModeHandler.disableGameMode(channel);
+						System.gc();
+					}
+					else
+					{	
+						MessageHandler.respond(event, "This channel is currently not playing a game.");
+					}
 				}
 			}
-			else if(cmdName.equalsIgnoreCase("@game") && Operators.isOp(event))
+			else if(cmdName.equalsIgnoreCase("@game"))
 			{
-				if(!GameModeHandler.isPlaying(channel))
+				if(Operators.isOp(event))
 				{
-					if(args.length == 1)
+					if(!GameModeHandler.isPlaying(channel))
 					{
-						boolean installed = false;
-						for(IGame game : GameModeHandler.games)
+						if(args.length == 1)
 						{
-							if(args[0].equalsIgnoreCase(game.getName()))
+							boolean installed = false;
+							for(IGame game : GameModeHandler.games)
 							{
-								installed = true;
-								GameModeHandler.enableGameMode(channel, args[0]);
+								if(args[0].equalsIgnoreCase(game.getName()))
+								{
+									installed = true;
+									GameModeHandler.enableGameMode(channel, args[0]);
+								}
+							}
+							if(!installed)
+							{
+								MessageHandler.respond(event, "That is not a valid game.");
+								MessageHandler.sendChannelMessage(event, "Currently installed: " + GameModeHandler.listAllGames());
 							}
 						}
-						if(!installed)
+						else
 						{
-							MessageHandler.respond(event, "That is not a valid game.");
+							MessageHandler.respond(event, "Please pass an installed game that you want to play.");
 							MessageHandler.sendChannelMessage(event, "Currently installed: " + GameModeHandler.listAllGames());
 						}
 					}
 					else
 					{
-						MessageHandler.respond(event, "Please pass an installed game that you want to play.");
-						MessageHandler.sendChannelMessage(event, "Currently installed: " + GameModeHandler.listAllGames());
+						MessageHandler.respond(event, "This channel is already playing a game!");
 					}
-				}
-				else
-				{
-					MessageHandler.respond(event, "This channel is already playing a game!");
 				}
 			}
 			else if(GameModeHandler.isPlaying(channel))
