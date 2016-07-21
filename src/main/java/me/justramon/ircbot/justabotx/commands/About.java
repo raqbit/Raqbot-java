1ac46280bfca49aebb45d329b8a74c1c4418c554
JustRamon <justramon@outlook.com>
2016-07-21 16:52:33 +0200

    Added version to ?about - Switched IDE
    oh boy

--- src/main/java/me/justramon/ircbot/justabotx/commands/About.java
+++ src/main/java/me/justramon/ircbot/justabotx/commands/About.java
@@ -1,37 +1,34 @@
 package me.justramon.ircbot.justabotx.commands;
 
-import java.lang.management.ManagementFactory;
-
-import org.pircbotx.Colors;
-import org.pircbotx.hooks.events.MessageEvent;
-
+import me.justramon.ircbot.justabotx.core.Core;
 import me.justramon.ircbot.justabotx.core.ICommand;
 import me.justramon.ircbot.justabotx.util.MessageHandler;
 import me.justramon.ircbot.justabotx.util.TimeParser;
+import org.pircbotx.Colors;
+import org.pircbotx.hooks.events.MessageEvent;
+
+import java.lang.management.ManagementFactory;
 
 public class About implements ICommand<MessageEvent>
 {
 
-	@Override
 	public void exe(MessageEvent event, String[] args) throws Exception
 	{
 		MessageHandler.sendChannelMessage(event, "Bleep Bleep - \"I am an IRC-Bot created by JustRamon\"");
+		MessageHandler.sendChannelMessage(event, "version: " + Core.version);
 		MessageHandler.sendChannelMessage(event, "Uptime: " + Colors.BOLD + new TimeParser().lts(ManagementFactory.getRuntimeMXBean().getUptime(), "%s:%s:%s:%s"));
 	}
 
-	@Override
 	public String[] getAliases()
 	{
 		return new String[] {"about"};
 	}
 
-	@Override
 	public String getInfo()
 	{
 		return "Gives you some info about the bot.";
 	}
 	
-	@Override
 	public boolean xtraFunc()
 	{
 		return false;
