c2b0654353065618b8b7829f5020ad3143b6d5fc
stepie22 <stepie22@gmail.com>
2016-10-05 21:57:56 +0200

    Aliases and more checks

--- src/main/java/me/justramon/ircbot/raqbotx/commands/Operator.java
+++ src/main/java/me/justramon/ircbot/raqbotx/commands/Operator.java
@@ -2,11 +2,11 @@ package me.justramon.ircbot.raqbotx.commands;
 
 import me.justramon.ircbot.raqbotx.config.ConfigHandler;
 import me.justramon.ircbot.raqbotx.core.ICommand;
-import me.justramon.ircbot.raqbotx.util.MessageHandler;
 import me.justramon.ircbot.raqbotx.util.StringUtils;
 import org.pircbotx.User;
 import org.pircbotx.hooks.events.MessageEvent;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Operator implements ICommand<MessageEvent> {
@@ -49,22 +49,39 @@ public class Operator implements ICommand<MessageEvent> {
                 break;
 
             case "remove":
+            case "rm":
                 if (args.length < 2) {
                     sendpm(event.getUser(), "Please give a username to add to the bot operator list!");
                     return;
                 }
 
+                ArrayList<String> actuallyRemoved = new ArrayList<>();
+                ArrayList<String> notRemoved = new ArrayList<>();
                 for (int i = 1; i < args.length; i++) {
                     if (args[i].isEmpty())
                         continue;
 
+                    // Because we can attempt to remove people not on the list
+                    if (ConfigHandler.config.operators.contains(args[i])) {
+                        actuallyRemoved.add(args[i]);
+                    }
+                    else {
+                        notRemoved.add(args[i]);
+                    }
+
+                    // It'll just ignore it anyway if it isn't in the list
                     ConfigHandler.config.operators.remove(args[i]);
                 }
 
                 ConfigHandler.save();
 
-                String removedPeople = StringUtils.arrayToString(StringUtils.trimArgrumentsFromCommand(args));
+                String removedPeople = StringUtils.joinNiceString(actuallyRemoved);
                 sendpm(event.getUser(),  "Removed " + removedPeople + " from the bot operator list");
+
+                if (notRemoved.size() != 0) {
+                    String notRemovedPeople = StringUtils.joinNiceString(notRemoved);
+                    sendpm(event.getUser(), "The following people were not in the operators list: " + notRemovedPeople);
+                }
                 break;
 
             default:
@@ -74,7 +91,7 @@ public class Operator implements ICommand<MessageEvent> {
 
     @Override
     public String[] getAliases() {
-        return new String[]{"operator"};
+        return new String[]{"operator", "op"};
     }
 
     @Override
